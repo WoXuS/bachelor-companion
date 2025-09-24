79143b19dca5761e1fd59f85a9125d11316f20a9
Kamil <camerzysta@gmail.com>
2025-09-24 17:42:41 +0200

    missed last endpoint

--- src/app/api/easter-eggs/placement/[key]/route.ts
+++ src/app/api/easter-eggs/placement/[key]/route.ts
@@ -1,10 +1,13 @@
-import { NextResponse } from 'next/server'
+import {NextRequest, NextResponse} from 'next/server'
 import { getEggByPlacement } from '@/server/db/services/easter-eggs.service'
 import {errMsg} from "@/lib/error";
+import {Ctx, getParams} from "@/types/api";
 
-export async function GET(_req: Request, { params }: { params: { key: string } }) {
+export async function GET(_req: NextRequest, ctx: Ctx<{ key: string }>) {
+    const { key } = await getParams(ctx)
+
     try {
-        const egg = await getEggByPlacement(params.key)
+        const egg = await getEggByPlacement(key)
         return NextResponse.json(egg ?? null)
     } catch (e: unknown) {
         return NextResponse.json({ message: errMsg(e) }, { status: 500 })
